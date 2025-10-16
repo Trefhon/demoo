e70dd1baebd67788f335971de38d3225c6593fbf
Trefhon <166126191+Trefhon@users.noreply.github.com>
2025-10-16 14:27:14 +0300

    Add files via upload

---
+++ Pages/ContentSelector.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContentVisorDemo.Pages
+{
+    public class ContentSelectorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/GlobalDashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContentVisorDemo.Pages
+{
+    public class GlobalDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
